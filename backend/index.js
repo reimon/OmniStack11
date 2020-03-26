cf995f50815dde907827df86b7573e166f2bd316
reimon <reimon@Reimons-iMac.local>
2020-03-25 21:00:32 -0300

    npx create-react-app frontend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ Evento: 'Semana Omnistack 11', Aluno: 'Reimon' });
+  return response.json({ Evento: 'Semana Omnistack -  11', Aluno: 'Reimon' });
 });
 app.listen(3333);
