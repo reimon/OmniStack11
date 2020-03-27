c8fdedd4d6243438ca66eb3d8b8aef17a77d878a
reimon <reimon@Reimons-iMac.local>
2020-03-26 22:05:14 -0300

    create controllers Profile and Sessions

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,20 @@ const express = require('express');
 const connection = require('./database/connection');
 
 const OngController = require('./controllers/OngController');
-
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.post('/sessions', SessionController.store);
+
 module.exports = routes;
