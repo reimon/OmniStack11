78416203fbc9261dd85d788626bc720a47ea1f1f
reimon <reimon@Reimons-iMac.local>
2020-03-30 21:05:49 -0300

    config google cloud

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -39,7 +39,7 @@ module.exports = {
       .select('ong_id')
       .first();
     if (incident.ong_id !== ong_id) {
-      return response.status(401).json({ error: 'Operation not permitted' });
+      return response.status(401).json({ error: ' - Operation not permitted' });
     }
 
     await connection('incidents')
